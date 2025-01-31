a931f4dbaa6ca94258b3c554d467335018f3a76d
Jeremy6541 <202071497@cegepsherbrooke.qc.ca>
2025-01-31 13:49:24 -0500

    Affichage type variable avec nom
    Affichage du type de la variable avec son nom en avant

---
+++ #variable à imprimer avec commande print.py
@@ -0,0 +1,11 @@
+#variable à imprimer avec commande print
+x = 10
+y = 3.14
+z = "bonjour"
+a = True
+
+#impression des variables
+print("x= ",type(x))
+print("y= ",type(y))
+print("z= ",type(z))
+print("a= ",type(a))
